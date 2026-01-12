88db6ea21e8e5b04639f4f93bb4e274a6689bbec
Khavar Facey <khavarfaceyu@gmail.com>
2026-01-11 23:13:28 -0500

    Added current season view

---
+++ khavarlist/Models/HomeView.cs
@@ -0,0 +1,11 @@
+﻿using khavarlist.Controllers;
+
+namespace khavarlist.Models
+{
+    public class HomeView
+    {
+        public List<JikanAnimeData>? TopAnime { get; set; }
+        public List<JikanMangaData>? TopManga { get; set; }
+        public List<JikanAnimeData>? CurrentAnime { get; set; }
+    }
+}
